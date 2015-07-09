dee8685eee51d6b62d2eaa968b71d4f58ea5a7b0
tonypai <tony77794@gmail.com>
2015-07-09 18:27:33 +0800

    Project init and build scene

---
+++ Assets/Model/Barrel/Scripts/BreakableObject.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreakableObject : MonoBehaviour {
+
+	public float explosionForce = 300f;
+	public float disappearDuration = 3f;
+
+	void Broken (Vector3 point) {
+
+		GetComponent<AudioSource> ().Play ();
+		transform.Find ("Barrel").GetComponent<MeshRenderer> ().enabled = false;
+
+		for (int i=1;i<transform.childCount;i++) {
+			transform.GetChild(i).gameObject.GetComponent<Rigidbody>().useGravity = true;
+			transform.GetChild(i).gameObject.GetComponent<MeshCollider>().enabled = true;
+			transform.GetChild(i).gameObject.GetComponent<Rigidbody>().AddForce ((transform.position - point) * explosionForce);
+		}
+
+		Invoke ("DestroyFragments", disappearDuration);
+	}
+
+	void DestroyFragments () {
+		Destroy (transform.gameObject);
+	}
+}
