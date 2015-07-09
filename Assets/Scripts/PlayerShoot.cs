2da902627397e792112fae836ab2131f3ccc0888
tonypai <tony77794@gmail.com>
2015-07-09 19:18:02 +0800

    Add debug ray and hide cursor

---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShoot : MonoBehaviour {
+
+	Transform gunTop;
+
+	void Start () {
+		Cursor.visible = false;
+		gunTop = transform.Find ("Character/Weapon/GunTop");
+	}
+	
+	void Update () {
+		Debug.DrawRay (
+			gunTop.position,
+			gunTop.forward * 100f,
+			Color.green
+		);
+	}
+}
