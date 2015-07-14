75b37c1dbd23f7be261d230f650bb4a5c46e97c1
tonypai <tony77794@gmail.com>
2015-07-14 20:20:57 +0800

    Switch weapon

---
+++ Assets/Scripts/Backpack.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Backpack : MonoBehaviour {
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			transform.Find ("Character/M16").gameObject.SetActive (false);
+			transform.Find ("Character/Magnum").gameObject.SetActive (true);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Alpha3)) {
+			transform.Find ("Character/Magnum").gameObject.SetActive (false);
+			transform.Find ("Character/M16").gameObject.SetActive (true);
+		}
+	}
+}
