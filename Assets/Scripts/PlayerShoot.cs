f0ef0a0431beb5c0bfaa45ba9234472e2a199848
tonypai <tony77794@gmail.com>
2015-07-14 18:52:36 +0800

    Modify code for switching weapon

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -3,50 +3,22 @@ using System.Collections;
 
 public class PlayerShoot : MonoBehaviour {
 
-	public GameObject bulletHolePrefab;
-	public GameObject muzzleFlashPrefab;
-	public GameObject bulletShellPrefab;
-	public float fireRate = .1f;
-
 	bool holdFire = false;
-	Transform gunTop;
 
 	void Start () {
 		Cursor.visible = false;
-		gunTop = transform.Find ("Character/Weapon/GunTop");
 	}
 	
 	void Update () {
-
 		if (!holdFire && Input.GetButton ("Fire1")) {
 			StartCoroutine ("Fire");
 		}
-
-		Debug.DrawRay (gunTop.position, gunTop.forward * 100f, Color.green);
 	}
 
 	IEnumerator Fire () {
 		holdFire = true;
-
-		if (GetComponentInChildren<Weapon> ().AddBullet (-1)) {
-			GetComponentInChildren<WeaponSound> ().PlaySound ("Bullet");
-
-			Destroy (Instantiate (muzzleFlashPrefab, gunTop.position, Quaternion.FromToRotation (Vector3.forward, gunTop.forward)), .5f);
-			Destroy (Instantiate (bulletShellPrefab, gunTop.position, Quaternion.identity), 2f);
-
-			RaycastHit hit = new RaycastHit ();
-			Ray ray = new Ray (gunTop.position, gunTop.forward);
-
-			if (Physics.Raycast (ray, out hit, 100f)) {
-				if (hit.collider.tag == "Wall" || hit.collider.tag == "Ground") {
-					Destroy (Instantiate (bulletHolePrefab, hit.point, Quaternion.FromToRotation (hit.collider.transform.forward, hit.normal)), 2f);
-				}
-			}
-		}
-		else
-			GetComponentInChildren<WeaponSound> ().PlaySound ("NoAmmo");
-
-		yield return new WaitForSeconds (fireRate);
+		GetComponentInChildren<Weapon> ().AddBullet (-1);
+		yield return new WaitForSeconds (GetComponentInChildren<Weapon>().fireSpeed);
 		holdFire = false;
 	}
 }
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -3,19 +3,49 @@ using System.Collections;
 
 public class Weapon : MonoBehaviour {
 
+	Transform gunTop;
+
+	public GameObject bulletHitPrefab;
+	public GameObject muzzleFlashPrefab;
+	public GameObject bulletShellPrefab;
+
+	public float fireSpeed = .1f;
 	public int bulletCount;
 	public int bulletMaxCount = 30;
 
 	void Start () {
 		bulletCount = bulletMaxCount;
+		gunTop = transform.Find ("GunTop");
+	}
+
+	void FixedUpdate () {
+		Debug.DrawRay (gunTop.position, gunTop.forward * 100f, Color.green);
+	}
+
+	void Attack () {
+		GetComponent<WeaponSound> ().PlaySound ("Bullet");
+		
+		Destroy (Instantiate (muzzleFlashPrefab, gunTop.position, Quaternion.FromToRotation (Vector3.forward, gunTop.forward)), .5f);
+		Destroy (Instantiate (bulletShellPrefab, gunTop.position, Quaternion.identity), 2f);
+		
+		RaycastHit hit = new RaycastHit ();
+		Ray ray = new Ray (gunTop.position, gunTop.forward);
+		
+		if (Physics.Raycast (ray, out hit, 100f)) {
+			if (hit.collider.tag == "Wall" || hit.collider.tag == "Ground") {
+				Destroy (Instantiate (bulletHitPrefab, hit.point, Quaternion.FromToRotation (hit.collider.transform.forward, hit.normal)), 2f);
+			}
+		}
 	}
 	
 	public bool AddBullet (int amt) {
 		bulletCount += amt;
 		if (bulletCount <= 0) {
 			bulletCount = 0;
+			GetComponent<WeaponSound> ().PlaySound ("NoAmmo");
 			return false;
 		}
+		Attack ();
 		return true;
 	}
 }
