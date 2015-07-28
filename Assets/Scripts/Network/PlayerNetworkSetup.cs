16d5e36f992af13a0054f00b1024354ff7f7459c
tonypai <tony77794@gmail.com>
2015-07-28 17:09:18 +0800

    Add Unity networking that includes player position and rotation

---
+++ Assets/Scripts/Network/PlayerNetworkSetup.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerNetworkSetup : NetworkBehaviour {
+	
+	[SerializeField] Camera fpsCamera;
+	[SerializeField] AudioListener audioListener;
+	
+	void Start () {
+		if (isLocalPlayer) {
+			// turn off scene camera
+			GameObject.Find ("SceneCamera").SetActive (false);
+			// turn character on
+			fpsCamera.enabled = true;
+			audioListener.enabled = true;
+			GetComponent <CharacterController>().enabled = true;
+			GetComponent <UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enabled = true;
+		}
+	}
+}
---
+++ Assets/Scripts/Network/PlayerSyncTransform.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerSyncTransform : NetworkBehaviour {
+	
+	[SyncVar] Vector3 syncPos;
+	[SyncVar] Quaternion syncCamRot;
+	[SyncVar] Quaternion syncCharRot;
+	
+	[SerializeField] Transform myTransform;
+	[SerializeField] Transform camTransform;
+	[SerializeField] float lerpRate = 15;
+	
+	void FixedUpdate () {
+		TransmitPosRot ();
+		LerpPosRot ();
+	}
+	
+	void LerpPosRot () {
+		if (!isLocalPlayer) {
+			myTransform.position = Vector3.Lerp (myTransform.position, syncPos, Time.deltaTime * lerpRate);
+			myTransform.rotation = Quaternion.Slerp (myTransform.rotation, syncCharRot, Time.deltaTime * lerpRate);
+			camTransform.rotation = Quaternion.Slerp (camTransform.rotation, syncCamRot, Time.deltaTime * lerpRate);
+		}
+	}
+	
+	[Command]
+	void CmdProvidePosRotToServer (Vector3 pos, Quaternion charRot, Quaternion camRot) {
+		syncPos = pos;
+		syncCharRot = charRot;
+		syncCamRot = camRot;
+	}
+	
+	[Client]
+	void TransmitPosRot () {
+		if (isLocalPlayer) {
+			CmdProvidePosRotToServer (myTransform.position, myTransform.rotation, camTransform.rotation);
+		}
+	}
+}
