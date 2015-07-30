1c4a1a366dca7d57c781c1787a6d65db1e644002
tonypai <tony77794@gmail.com>
2015-07-30 13:45:39 +0800

    Reduce update time

--- Assets/Scripts/Network/PlayerSyncTransform.cs
+++ Assets/Scripts/Network/PlayerSyncTransform.cs
@@ -5,37 +5,56 @@ using UnityEngine.Networking;
 public class PlayerSyncTransform : NetworkBehaviour {
 	
 	[SyncVar] Vector3 syncPos;
+	[SyncVar] Quaternion syncMyRot;
 	[SyncVar] Quaternion syncCamRot;
-	[SyncVar] Quaternion syncCharRot;
 	
 	[SerializeField] Transform myTransform;
 	[SerializeField] Transform camTransform;
-	[SerializeField] float lerpRate = 15;
 	
-	void FixedUpdate () {
-		TransmitPosRot ();
+	float lerpRate = 15;
+
+	Vector3 lastPos;
+	float posThreshold = .5f;
+	Quaternion lastMyRot;
+	Quaternion lastCamRot;
+	float rotThreshold = 5f;
+
+	void Update () {
 		LerpPosRot ();
 	}
-	
+
+	void FixedUpdate () {
+		TransmitPosRot ();
+	}
+
 	void LerpPosRot () {
 		if (!isLocalPlayer) {
 			myTransform.position = Vector3.Lerp (myTransform.position, syncPos, Time.deltaTime * lerpRate);
-			myTransform.rotation = Quaternion.Slerp (myTransform.rotation, syncCharRot, Time.deltaTime * lerpRate);
+			myTransform.rotation = Quaternion.Slerp (myTransform.rotation, syncMyRot, Time.deltaTime * lerpRate);
 			camTransform.rotation = Quaternion.Slerp (camTransform.rotation, syncCamRot, Time.deltaTime * lerpRate);
 		}
 	}
-	
+
 	[Command]
 	void CmdProvidePosRotToServer (Vector3 pos, Quaternion charRot, Quaternion camRot) {
 		syncPos = pos;
-		syncCharRot = charRot;
+		syncMyRot = charRot;
 		syncCamRot = camRot;
 	}
 	
 	[Client]
 	void TransmitPosRot () {
 		if (isLocalPlayer) {
-			CmdProvidePosRotToServer (myTransform.position, myTransform.rotation, camTransform.rotation);
+			if (
+				Vector3.Distance (myTransform.position, lastPos) > posThreshold ||
+				Quaternion.Angle (myTransform.rotation, lastMyRot) > rotThreshold ||
+				Quaternion.Angle (camTransform.rotation, lastCamRot) > rotThreshold
+			) {
+				CmdProvidePosRotToServer (myTransform.position, myTransform.rotation, camTransform.rotation);
+				lastPos = myTransform.position;
+				lastMyRot = myTransform.rotation;
+				lastCamRot = camTransform.rotation;
+			}
 		}
 	}
 }
