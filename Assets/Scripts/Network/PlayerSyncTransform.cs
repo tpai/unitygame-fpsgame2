42d399ec98e3f9e613036790782fea1f44261597
tonypai <tony77794@gmail.com>
2015-07-30 14:33:11 +0800

    Latency simulation

--- Assets/Scripts/Network/PlayerSyncTransform.cs
+++ Assets/Scripts/Network/PlayerSyncTransform.cs
@@ -1,23 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.Networking;
+using System.Collections.Generic;
 
+[NetworkSettings (channel=0, sendInterval=.033f)]
 public class PlayerSyncTransform : NetworkBehaviour {
 	
-	[SyncVar] Vector3 syncPos;
-	[SyncVar] Quaternion syncMyRot;
-	[SyncVar] Quaternion syncCamRot;
+	[SyncVar (hook="SyncPosValues")] Vector3 syncPos;
+	[SyncVar (hook="SyncMyRotValues")] Quaternion syncMyRot;
+	[SyncVar (hook="SyncCamRotValues")] Quaternion syncCamRot;
 	
 	[SerializeField] Transform myTransform;
 	[SerializeField] Transform camTransform;
-	
-	float lerpRate = 15;
+
+	[SerializeField] bool useHistoricalLerping;
+	List<Vector3> syncPosList = new List<Vector3>();
+	List<Quaternion> syncMyRotList = new List<Quaternion>();
+	List<Quaternion> syncCamRotList = new List<Quaternion>();
+	float lerpPosRate;
+	float lerpRotRate;
+	float normalLerpRate = 16f;
+	float fasterLerpRate = 27f;
+	float posCloseEnough = .11f;
+	float rotCloseEnough = 5f;
 
 	Vector3 lastPos;
 	float posThreshold = .5f;
 	Quaternion lastMyRot;
 	Quaternion lastCamRot;
-	float rotThreshold = 5f;
+	float rotThreshold = 1f;
+
+	void Start () {
+		lerpPosRate = normalLerpRate;
+		lerpRotRate = normalLerpRate;
+	}
 
 	void Update () {
 		LerpPosRot ();
@@ -29,12 +45,66 @@ public class PlayerSyncTransform : NetworkBehaviour {
 
 	void LerpPosRot () {
 		if (!isLocalPlayer) {
-			myTransform.position = Vector3.Lerp (myTransform.position, syncPos, Time.deltaTime * lerpRate);
-			myTransform.rotation = Quaternion.Slerp (myTransform.rotation, syncMyRot, Time.deltaTime * lerpRate);
-			camTransform.rotation = Quaternion.Slerp (camTransform.rotation, syncCamRot, Time.deltaTime * lerpRate);
+			if (useHistoricalLerping)
+				HistoricalLerping ();
+			else
+				OrdinaryLerping ();
 		}
 	}
 
+	void OrdinaryLerping () {
+		myTransform.position = Vector3.Lerp (myTransform.position, syncPos, Time.deltaTime * lerpPosRate);
+		myTransform.rotation = Quaternion.Slerp (myTransform.rotation, syncMyRot, Time.deltaTime * lerpRotRate);
+		camTransform.rotation = Quaternion.Slerp (camTransform.rotation, syncCamRot, Time.deltaTime * lerpRotRate);
+	}
+
+	void HistoricalLerping () {
+		if (syncPosList.Count > 0) {
+			myTransform.position = Vector3.Lerp (myTransform.position, syncPosList[0], Time.deltaTime * lerpPosRate);
+
+			if (Vector3.Distance (myTransform.position, syncPosList[0]) < posCloseEnough) {
+				syncPosList.RemoveAt (0);
+			}
+
+			if (syncPosList.Count > 10) {
+				lerpPosRate = fasterLerpRate;
+			}
+			else {
+				lerpPosRate = normalLerpRate;
+			}
+		}
+
+		if (syncMyRotList.Count > 0) {
+			myTransform.rotation = Quaternion.Slerp (myTransform.rotation, syncMyRotList[0], Time.deltaTime * lerpRotRate);
+
+			if (Quaternion.Angle (myTransform.rotation, syncMyRotList[0]) < rotCloseEnough) {
+				syncMyRotList.RemoveAt (0);
+			}
+
+			if (syncMyRotList.Count > 10) {
+				lerpRotRate = fasterLerpRate;
+			}
+			else {
+				lerpRotRate = normalLerpRate;
+			}
+		}
+
+		if (syncCamRotList.Count > 0) {
+			camTransform.rotation = Quaternion.Slerp (camTransform.rotation, syncCamRotList[0], Time.deltaTime * lerpRotRate);
+
+			if (Quaternion.Angle (camTransform.rotation, syncCamRotList[0]) < rotCloseEnough) {
+				syncCamRotList.RemoveAt (0);
+			}
+
+			if (syncCamRotList.Count > 10) {
+				lerpRotRate = fasterLerpRate;
+			}
+			else {
+				lerpRotRate = normalLerpRate;
+			}
+		}
+	}
+		
 	[Command]
 	void CmdProvidePosRotToServer (Vector3 pos, Quaternion charRot, Quaternion camRot) {
 		syncPos = pos;
@@ -56,5 +126,25 @@ public class PlayerSyncTransform : NetworkBehaviour {
 				lastCamRot = camTransform.rotation;
 			}
 		}
+	}
+
+	// ==========
+
+	[Client]
+	void SyncPosValues (Vector3 latestPos) {
+		syncPos = latestPos;
+		syncPosList.Add (latestPos);
+	}
+
+	[Client]
+	void SyncMyRotValues (Quaternion latestRot) {
+		syncMyRot = latestRot;
+		syncMyRotList.Add (latestRot);
+	}
+
+	[Client]
+	void SyncCamRotValues (Quaternion latestRot) {
+		syncCamRot = latestRot;
+		syncCamRotList.Add (latestRot);
 	}
 }
