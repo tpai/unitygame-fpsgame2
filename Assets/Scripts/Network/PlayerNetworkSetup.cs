c0eb97ceb986eea4581f8647f57398c5b35cc316
tonypai <tony77794@gmail.com>
2015-08-01 23:13:01 +0800

    Set unique ID when start

--- Assets/Scripts/Network/PlayerNetworkSetup.cs
+++ Assets/Scripts/Network/PlayerNetworkSetup.cs
@@ -3,10 +3,51 @@ using System.Collections;
 using UnityEngine.Networking;
 
 public class PlayerNetworkSetup : NetworkBehaviour {
-	
+
+	[SyncVar] public string playerUniqueName;
+	private NetworkInstanceId playerNetID;
+	private Transform myTransform;
+
+	public override void OnStartLocalPlayer ()
+	{
+		GetNetIdentity ();
+		SetIdentity ();
+	}
+
+	[Client]
+	void GetNetIdentity () {
+		playerNetID = GetComponent<NetworkIdentity> ().netId;
+		CmdTellServerMyIdentity ("Player " + playerNetID.ToString ());
+	}
+
+	[Client]
+	void SetIdentity () {
+		if (!isLocalPlayer) {
+			myTransform.name = playerUniqueName;
+			myTransform.tag = "Enemy";
+		} else {
+			myTransform.name = "Player " + playerNetID.ToString ();
+		}
+	}
+
+	[Command]
+	void CmdTellServerMyIdentity (string identity) {
+		playerUniqueName = identity;
+	}
+
+	void Update () {
+		if (myTransform.name == "" || myTransform.name == "Player(Clone)") {
+			SetIdentity ();
+		}
+	}
+
 	[SerializeField] Camera fpsCamera;
 	[SerializeField] AudioListener audioListener;
-	
+
+	void Awake () {
+		myTransform = transform;
+	}
+
 	void Start () {
 		if (isLocalPlayer) {
 			// turn off scene camera
